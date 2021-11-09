f5b54f142c542af3e05c9b87f184b3e57bfc5e08
MaksymM92 <maksym.mostovyi.ua@gmail.com>
2021-11-09 22:36:51 +0400

    Second step

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 program.parse(process.argv);
