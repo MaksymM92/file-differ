d69ff6467b4ae621a5b41ec12840407bb10b7521
MaksymM92 <maksym.mostovyi.ua@gmail.com>
2021-11-19 17:00:51 +0400

    Step 6

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,20 +10,20 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const simpleDiffResult = readFixture('result.txt');
-const complexDiffResult = readFixture('nestedJSONresult.txt');
+const result = readFixture('result.txt');
+const stylishResult = readFixture('stylish_result.txt');
 
-const testCases = [['firstFile', 'secondFile'], ['firstFile_v2', 'secondFile_v2']];
+const fileFormats = ['json', 'yml'];
 
 describe('gendiff', () => {
-  test.each(testCases)('compareJSONFiles', (files) => {
-    const file1 = getFixturePath(`${files[0]}.json`);
-    const file2 = getFixturePath(`${files[1]}.json`);
-    expect(showDiff(file1, file2)).toEqual(simpleDiffResult);
-  });
-  test.each(testCases)('compareYAMLFiles', (files) => {
-    const file1 = getFixturePath(`${files[0]}.yml`);
-    const file2 = getFixturePath(`${files[1]}.yml`);
-    expect(showDiff(file1, file2)).toEqual(complexDiffResult);
+  test.each(fileFormats)('compareFiles', (format) => {
+    const file1 = getFixturePath(`firstFile.${format}`);
+    const file2 = getFixturePath(`secondFile.${format}`);
+    const file3 = getFixturePath(`firstFile_v2.${format}`);
+    const file4 = getFixturePath(`secondFile_v2.${format}`);
+    console.log('file3', file3);
+    console.log('file4', file4);
+    expect(showDiff(file1, file2)).toEqual(result);
+    expect(showDiff(file3, file4, 'stylish')).toEqual(stylishResult);
   });
 });
