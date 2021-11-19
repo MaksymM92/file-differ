dab6fc0ed3d5a45d7734e08219449ce2a5625b96
MaksymM92 <maksym.mostovyi.ua@gmail.com>
2021-11-19 20:50:35 +0400

    8 step. Renamed files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { showDiff } from '../src/app.js';
+import { showDiff } from '../src';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { runProgramme } from '../src/app.js';
+import { runProgramme } from '../src/index.js';
 
 runProgramme();
