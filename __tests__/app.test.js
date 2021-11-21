f05466c2785c8973e0e98d32e9da97bfb46a563d
MaksymM92 <maksym.mostovyi.ua@gmail.com>
2021-11-21 21:05:50 +0400

    refactoring

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { showDiff } from '../src';
+import showDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
